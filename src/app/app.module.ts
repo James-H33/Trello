3818262b86ba9f14a08bab3580e7029a821b1512
James Hall-Treworgy <jameshall-treworgy@Jamess-MacBook-Pro.local>
2018-02-09 15:22:29 -0500

    Added Toggle Input Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BoardComponent } from '../board/board-component';
 import { ListComponent } from '../board/list/list-component';
+import { CardComponent } from '../board/card/card.component';
 import { NavigationComponent } from '../navigation/navigation-component';
-import { ListItemComponent } from '../board/list-item/list-item.component';
+import { ToggleInputComponent } from '../toggle-input/toggle-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
     ListComponent,
-    ListItemComponent,
+    CardComponent,
+    ToggleInputComponent,
     NavigationComponent
   ],
   imports: [
